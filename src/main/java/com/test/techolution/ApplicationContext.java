bdaf02e3d84a9dcfe342c9f0f5fb1737b1029089
Rajeev <rajeevkr@rssoftware.co.in>
2017-01-06 22:06:51 +0530

    upd

---
+++ src/main/java/com/test/techolution/ApplicationContext.java
@@ -0,0 +1,35 @@
+package com.test.techolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+
+/**
+ * @author Rajeev
+ *
+ */
+@Configuration
+@EnableAutoConfiguration
+
+@SpringBootApplication
+@EnableScheduling
+
+
+public class ApplicationContext extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ApplicationContext.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(ApplicationContext.class, args);
+    }
+    
+    
+}
